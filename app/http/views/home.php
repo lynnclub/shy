5841282077021c7383682cce3900684a278b338c
Lynn <caoyuanzhi@top-list.com.cn>
2018-12-06 23:39:45 +0800

    统计信息

--- app/http/views/home.php
+++ app/http/views/home.php
@@ -1,3 +1,7 @@
 <div id="hello-world">
     <?php param('info') ?>
 </div>
+<div id="system">
+    <p>Memory Peak: <?php echo memory_get_peak_usage() / 1024; ?> kb</p>
+    <p>Used Time: <?php echo microtime(true) - SHY_START; ?> second</p>
+</div>
