32d184f4737f6edd0138507cf90b86cf62ee4b40
caoyuanzhi <caoyuanzhi@top-list.com.cn>
2021-09-01 10:34:15 +0800

    command

--- config/command.php
+++ config/command.php
@@ -6,7 +6,8 @@ return [
      */
     'list' => [Shy\Command\Shy::class => 'commandList'],
     'env' => [Shy\Command\Shy::class => 'env'],
-    'route' => [Shy\Command\Shy::class => 'routeIndex'],
+    'route' => [Shy\Command\Shy::class => 'routeConfig'],
+    'route_index' => [Shy\Command\Shy::class => 'routeIndex'],
     'workerman' => [Shy\Command\WorkerMan::class => 'workerMan'],
     'http_workerman' => [Shy\Command\WorkerMan::class => 'httpWorkerMan'],
 
